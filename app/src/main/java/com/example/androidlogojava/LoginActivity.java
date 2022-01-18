a9539e16b1f3cee6df2e6a160402083313a69391
carlos <carlos.olea.4.11@gmail.com>
2022-01-17 23:45:29 -0600

    Se realizo una animacion con una imagen y un nombre donde se genera un desplazamiento que se llama Splash screem

---
+++ app/src/main/java/com/example/androidlogojava/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidlogojava;
+
+import static com.example.androidlogojava.R.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/androidlogojava/MainActivity.java
+++ app/src/main/java/com/example/androidlogojava/MainActivity.java
@@ -2,16 +2,51 @@ package com.example.androidlogojava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
+        // animaciones
+
+        Animation animation1 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_arriba);
+        Animation animation2 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_abajo);
+
+
+        TextView textViewCodeGeas= findViewById(R.id.codeGeastextView);
+        TextView  textViewDg = findViewById(R.id.dgtextView);
+        ImageView loImageView = findViewById(R.id.imageView);
+
+        textViewDg.setAnimation(animation2);
+        textViewCodeGeas.setAnimation(animation2);
+        loImageView.setAnimation(animation1);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
+
     }
-    int carlos =0;
-    // se sigue realizando la prueba de commit
+
 
 }
